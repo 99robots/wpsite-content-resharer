f42e89ca32e00e2c6b8a8310cf1e2b3aa5ec3718
Kyle <kjbenk@gmail.com>
2014-07-27 12:01:26 -0400

    Updated Settings
    Adding styling to Sign button

--- include/connect-accounts/connect-twitter.php
+++ include/connect-accounts/connect-twitter.php
@@ -8,7 +8,7 @@ if (!isset($settings['twitter']['token']) || $settings['twitter']['token'] == ''
 
 	$redirect_url = $connection->getAuthorizeURL($temporary_credentials);
 
-	?><a href="<?php echo $redirect_url; ?>"><?php _e('Sign In', WPSITE_TWITTER_RESHARE_PLUGIN_TEXT_DOMAIN); ?></a><?php
+	?><a href="<?php echo $redirect_url; ?>"><input class="button" type="button" value="<?php _e('Sign In', WPSITE_TWITTER_RESHARE_PLUGIN_TEXT_DOMAIN); ?>"/></a><?php
 
 	$settings['twitter']['token'] = $temporary_credentials['oauth_token'];
 	$settings['twitter']['token_secret'] = $temporary_credentials['oauth_token_secret'];
@@ -39,6 +39,12 @@ if (!isset($settings['twitter']['token']) || $settings['twitter']['token'] == ''
 
 } else {
 
+	$sign_in = new TwitterOAuth(self::$api_key, self::$api_secret);
+
+	$temporary_credentials = $sign_in->getRequestToken('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+
+	$redirect_url = $sign_in->getAuthorizeURL($temporary_credentials);
+
 	$connection = new TwitterOAuth(self::$api_key, self::$api_secret, $settings['twitter']['token'],
 	$settings['twitter']['token_secret']);
 
@@ -53,22 +59,23 @@ if (!isset($settings['twitter']['token']) || $settings['twitter']['token'] == ''
 			$settings_all['accounts']['twitter'] = $settings;
 
 			update_option('wpsite_twitter_reshare_settings', $settings_all);
+
+			?><a href="<?php echo $redirect_url; ?>"><input class="button" type="button" value="<?php _e('Sign In', WPSITE_TWITTER_RESHARE_PLUGIN_TEXT_DOMAIN); ?>"/></a><?php
+		} else {
+			?>
+			<div class="<?php echo self::$prefix_dash; ?>container">
+				<div class="<?php echo self::$prefix_dash; ?>profile-image">
+					<img src="<?php echo $account->profile_image_url; ?>" />
+				</div>
+				<div class="<?php echo self::$prefix_dash; ?>screen-name">
+					<a href="https://twitter.com/<?php echo $account->screen_name; ?>"><?php echo $account->screen_name; ?></a>
+				</div>
+				<div class="<?php echo self::$prefix_dash; ?>remove">
+					<a href="<?php echo wp_nonce_url($_SERVER['PHP_SELF'] . '?page=' . self::$account_dashboard_page . '&action=remove&account=' . $settings['id'] . '&type=twitter', 'wpsite_twitter_reshare_admin_settings_remove'); ?>"></a>
+				</div>
+			</div>
+			<?php
 		}
-
-		?>
-		<div class="<?php echo self::$prefix_dash; ?>container">
-			<div class="<?php echo self::$prefix_dash; ?>profile-image">
-				<img src="<?php echo $account->profile_image_url; ?>" />
-			</div>
-			<div class="<?php echo self::$prefix_dash; ?>screen-name">
-				<a href="https://twitter.com/<?php echo $account->screen_name; ?>"><?php echo $account->screen_name; ?></a>
-			</div>
-			<div class="<?php echo self::$prefix_dash; ?>remove">
-				<a href="<?php echo wp_nonce_url($_SERVER['PHP_SELF'] . '?page=' . self::$account_dashboard_page . '&action=remove&account=' . $settings['id'] . '&type=twitter', 'wpsite_twitter_reshare_admin_settings_remove'); ?>"></a>
-			</div>
-		</div>
-		<?php
-
 	} else {
 		$settings['twitter']['token'] = '';
 		$settings['twitter']['token_secret'] = '';
@@ -76,5 +83,7 @@ if (!isset($settings['twitter']['token']) || $settings['twitter']['token'] == ''
 		$settings_all['accounts']['twitter'] = $settings;
 
 		update_option('wpsite_twitter_reshare_settings', $settings_all);
+
+		?><a href="<?php echo $redirect_url; ?>"><input class="button" type="button" value="<?php _e('Sign In', WPSITE_TWITTER_RESHARE_PLUGIN_TEXT_DOMAIN); ?>"/></a><?php
 	}
 }
