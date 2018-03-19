d025c824d892b9020f491f83782e613163f64388
Jay17231 <gajjarjay23@gmail.com>
2018-03-20 03:01:46 +0530

    Update: Bug Fixes and Updates
    1. Fixed fatal error
    2. Updated twitter share character length to 280

--- include/reshare-accounts/reshare-twitter.php
+++ include/reshare-accounts/reshare-twitter.php
@@ -1,19 +1,19 @@
 <?php
 require_once(WPSITE_TWITTER_RESHARE_PLUGIN_DIR . self::$api_twitter_dir);
 
-\Codebird\Codebird::setConsumerKey(WPsiteTwitterReshare::$api_key, WPsiteTwitterReshare::$api_secret);
+\Codebird\Codebird::setConsumerKey(WPsite_Content_Resharer::$api_key, WPsite_Content_Resharer::$api_secret);
 $cb = \Codebird\Codebird::getInstance();
 $cb->setToken($account['twitter']['token'], $account['twitter']['token_secret']);
 
 if (isset($account['general']['featured_image']) && isset($featured_image) && $featured_image != ''/* && (isset($params['media[]']) && $params['media[]'] != '') */) {
 	$params = array(
-	    'status' 	=> strip_tags(substr($content, 0, 139)),
+	    'status' 	=> strip_tags(substr($content, 0, 279)),
 	    'media[]' 	=> file_get_contents($featured_image)
 	);
 
 	$reply = $cb->statuses_updateWithMedia($params);
 } else {
-	$reply = $cb->statuses_update('status=' . strip_tags(substr($content, 0, 139)));
+	$reply = $cb->statuses_update('status=' . strip_tags(substr($content, 0, 279)));
 }
 
 //error_log(serialize($reply));
